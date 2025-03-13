49d002d9eb9bbc08f6370ea87d45418ffe55196e
Resky89 <reskyprabowo89@gmail.com>
2025-03-13 09:46:21 +0700

    make new route for forget password and create new password

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Middleware\AuthMiddleware;
+use Illuminate\Http\Request;
 
 // Authentication Routes
 Route::get('/', function () {
@@ -31,3 +32,24 @@ Route::prefix('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 });
 */
+
+// Route untuk menampilkan form lupa password
+Route::get('/forgot-password', function () {
+    return view('Auth.forget_password');
+})->name('password.request');
+
+// Route untuk memproses form lupa password dan redirect ke create password
+Route::post('/forgot-password', function (Request $request) {
+    // Di sini bisa ditambahkan validasi email jika diperlukan
+    return redirect()->route('password.create');
+})->name('password.email');
+
+// Route untuk menampilkan form create password
+Route::get('/create-password', function () {
+    return view('Auth.create_password');
+})->name('password.create');
+
+// Route untuk memproses form create password
+Route::post('/create-password', function (Request $request) {
+    // Logic untuk memproses pembuatan password baru
+})->name('password.store');
