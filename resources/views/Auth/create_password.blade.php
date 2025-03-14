4fb2694aaa42a0ce2f8a3704465830747c5b4101
Resky89 <reskyprabowo89@gmail.com>
2025-03-14 10:05:36 +0700

    fix(auth): improve readability of input text in create password form
    - Change input text color from light gray to dark gray for better visibility
    - Keep placeholder text in light gray
    - Make entered passwords more clearly visible to users while typing

--- resources/views/Auth/create_password.blade.php
+++ resources/views/Auth/create_password.blade.php
@@ -58,7 +58,7 @@
                                     type="password"
                                     name="password"
                                     placeholder="Insert Your New Password"
-                                    class="w-full p-[12px_16px] border border-[#D9D9D9] rounded-lg font-['Inter'] text-sm text-[#B3B3B3] focus:outline-none"
+                                    class="w-full p-[12px_16px] border border-[#D9D9D9] rounded-lg font-['Inter'] text-sm text-[#303030] focus:outline-none placeholder:text-[#B3B3B3]"
                                     required
                                 >
                             </div>
@@ -69,7 +69,7 @@
                                     type="password"
                                     name="password_confirmation"
                                     placeholder="Confirm Your Password"
-                                    class="w-full p-[12px_16px] border border-[#D9D9D9] rounded-lg font-['Inter'] text-sm text-[#B3B3B3] focus:outline-none"
+                                    class="w-full p-[12px_16px] border border-[#D9D9D9] rounded-lg font-['Inter'] text-sm text-[#303030] focus:outline-none placeholder:text-[#B3B3B3]"
                                     required
                                 >
                             </div>
