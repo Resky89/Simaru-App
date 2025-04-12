c8857ccb038f98110bedff99a7299cc505f4cffc
Resky89 <reskyprabowo89@gmail.com>
2025-04-12 12:26:40 +0700

    refactor: rename ProcurementController to ProcurementRequestController

--- app/Http/Controllers/ProcurementRequestController.php
+++ app/Http/Controllers/ProcurementRequestController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\ApiService;
 
-class ProcurementController extends Controller
+class ProcurementRequestController extends Controller
 {
     protected $apiService;
 
